3ba3277338190d00fc175f75d86665ae28426cfe
Steve Doty <stevedoty16@gmail.com>
2021-05-20 10:53:59 -0700

    0.0.0.24 create intro comp

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './assets/css/main.css';
 import Navbar from './components/navbar.js';
 import Banner from './components/banner.js';
 import Intro from './components/intro.js';
+import Vision from './components/vision.js';
 import Gallery from './components/gallery.js';
 
 
@@ -18,60 +19,9 @@ function App() {
 
       <Intro />
 
+      <Vision />
 
 
-    			<section id="one" className="main style2">
-    				<div className="content box style2">
-    					<header>
-    						<h2>Oceans</h2>
-    					</header>
-    					<p>vastness and vibrancy</p>
-    				</div>
-
-    			</section>
-
-
-    			<section id="two" className="main style2">
-    				<div className="content box style2">
-    					<header>
-    						<h2>Forests</h2>
-    					</header>
-    					<p>tranquility and lushness</p>
-    				</div>
-
-    			</section>
-
-
-    			<section id="three" className="main style2">
-    				<div className="content box style2">
-    					<header>
-    						<h2>Air</h2>
-    					</header>
-    					<p>fresh breezes and clear skies</p>
-    				</div>
-
-    			</section>
-
-
-    			<section id="four" className="main style2">
-    				<div className="content box style2">
-    					<header>
-    						<h2>Sun</h2>
-    					</header>
-    					<p>warmth and abundance</p>
-    				</div>
-
-    			</section>
-
-
-    			<section id="five" className="main style2">
-    				<div className="content box style2">
-    					<header>
-    						<h2>Ecosystem</h2>
-    					</header>
-    					<p>a community of respect and cooperation</p>
-    				</div>
-    			</section>
 
 
 
