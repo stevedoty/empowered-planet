fc24986c00c88b304e8e426cdba0c179115d4ed2
Steve Doty <stevedoty16@gmail.com>
2021-07-30 13:32:13 -0700

    Donations JSX cleanup

--- src/components/donations.js
+++ src/components/donations.js
@@ -1,27 +1,49 @@
 import React from 'react';
 
-
+let donationsArray = [
+  {},
+  {
+    title:"Donations",
+    list: [
+      {
+        title:"",
+        link:"https://www.patreon.com/login",
+        icon:"fa-patreon",
+      },
+      {
+        title:"",
+        link:"https://www.kickstarter.com/login",
+        icon:"fa-kickstarter",
+      },
+      {
+        title:"",
+        link:"https://www.gofundme.com/login",
+        icon:"fa-patreon",
+      },
+      {
+        title:"",
+        link:"https://www.fundly.com/login",
+        icon:"fa-kickstarter",
+      },
+    ]
+  }
+]
 
 function Donations() {
   return (
     <div className="Donations BrandLinks-column">
 
-        <a
-          href="https://www.patreon.com/login"
-          target="_blank" rel="noopener noreferrer" className="BrandLinks-icon Donations-icon fa-patreon">
-          <span className="label"></span></a>
-        <a
-          href="https://www.kickstarter.com/login"
-          target="_blank" rel="noopener noreferrer" className="BrandLinks-icon Donations-icon fa-kickstarter">
-          <span className="label"></span></a>
-        <a
-          href="https://www.gofundme.com/login"
-          target="_blank" rel="noopener noreferrer" className="BrandLinks-icon Donations-icon fa-patreon">
-          <span className="label"></span></a>
-        <a
-          href="https://www.fundly.com/login"
-          target="_blank" rel="noopener noreferrer" className="BrandLinks-icon Donations-icon fa-kickstarter">
-          <span className="label"></span></a>
+        {donationsArray[1].list.map(item=>{
+          return(
+            <a
+              href={item.link}
+              target="_blank" rel="noopener noreferrer" className={"BrandLinks-icon Donations-icon brands "+item.icon}>
+              <span className="label">
+
+              </span>
+            </a>
+          )
+        })}
 
     </div>
   );
