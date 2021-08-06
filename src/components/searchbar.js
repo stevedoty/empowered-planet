0eef58fb349e073b4bf62e2d7ea7277eef9d87ad
Steve Doty <stevedoty16@gmail.com>
2021-08-06 08:55:45 -0700

    -

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 
 
 function Searchbar() {
+  const [searchTerm, setSearchTerm] = useState('')
   return (
     <div id="Searchbar" className="Searchbar">
 
-      <input className="Searchbar-content" type="text" name="search" placeholder="Search"/>
+      <input className="Searchbar-content" type="text" name="search" placeholder="search..." onChange={event => {setSearchTerm(event.target.value)}}/>
 
     </div>
   );
