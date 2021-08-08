29c3428f05ef93dcbb739e65218f9b965ba15534
Steve Doty <stevedoty16@gmail.com>
2021-08-08 11:04:18 -0700

    DB cleanup

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,67 +1,7 @@
+import MAINDATA from "./data.js"
 import React from 'react';
 
-let currentIssues = {
-  globalIssues:[
-    {
-      heading:"Global Issues",
-      subheading:""
-    },
-    {
-    title:"",
-    list:[
-      "Privatization and Pollution of Life’s Essential Resources",
-      "Genocide",
-      "Slavery",
-      "Sex Slavery and Kidnapping",
-      "Child Rape and Molestation",
-      "International Stealing of Resources",
-      "Oppression of Population Through Military",
-      "Private Control of Information Through Propaganda, Misinformation, and Censorship",
-      "Irreversible Damage to Our Public Environment By Private Interest Groups"
-      ]
-    }
-  ],
-  internationalIssues:[
-    {
-      heading:"International Issues",
-      subheading:""
-    },
-    {
-    title:"",
-    list:[]
-    }
-  ],
-  nationalIssues:[
-    {
-      heading:"National Issues",
-      subheading:""
-    },
-    {
-    title:"",
-    list:[]
-    }
-  ],
-  regionalIssues:[
-    {
-      heading:"Regional Issues",
-      subheading:""
-    },
-    {
-    title:"",
-    list:[]
-    }
-  ],
-  localIssues:[
-    {
-      heading:"Local Issues",
-      subheading:""
-    },
-    {
-    title:"",
-    list:[]
-    }
-  ],
-}
+let currentIssues = MAINDATA.CURRENTISSUESDATA
 
 function Intro() {
   return (
