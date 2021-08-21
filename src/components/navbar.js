8d69423359772a0833c5d1106957e7b5fbaee333
Steve Doty <stevedoty16@gmail.com>
2021-08-21 12:43:05 -0700

    update navbar filter

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,8 +7,7 @@ function Navbar() {
   return (
 
     <div id="Navbar" className="Navbar">
-        <div id="Navbar-filter" className="Navbar-filter"></div>
-        <div id="Navbar-filter" className="Navbar-filter"></div>
+        <div id="Navbar-filter" className="Navbar-filter"></div><div id="Navbar-filter" className="Navbar-filter"></div><div id="Navbar-filter" className="Navbar-filter"></div><div id="Navbar-filter" className="Navbar-filter"></div><div id="Navbar-filter" className="Navbar-filter"></div><div id="Navbar-filter" className="Navbar-filter"></div>
           <a href="" className="Navbar-item"><img src={brandIcon} className="Navbar-brand-icon" alt=""/></a>
           <a href="" className="Navbar-item"><p className="Navbar-brand-name">Empowered Planet</p></a>
           <a href="#Library" className="Navbar-item">Library</a>
