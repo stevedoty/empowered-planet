54d2f7652b65af0da9c8d884ec6506ce57929007
Steve Doty <stevedoty16@gmail.com>
2021-12-27 19:44:46 -0800

    -

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
     return (
       <ReduxProvider store={reduxStore}>
         <div className="App">
+        
           <Navbar />
           <Searchbar />
           <Banner />
