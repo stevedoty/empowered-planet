3c990c040d61e59e4b190d98c82e293e9e6cde3e
Steve Doty <stevedoty16@gmail.com>
2022-01-06 10:14:13 -0800

    quote in banner changes every render

--- src/components/banner.js
+++ src/components/banner.js
@@ -12,26 +12,24 @@ let quotesArray = [
     publisher: "E. P."
   },
 ]
-
+let quotesArrayIndex = Math.floor( Math.random()*(quotesArray.length)  )
+console.log(quotesArrayIndex);
+let quotesArrayItem = quotesArray[quotesArrayIndex]
+console.log(quotesArrayItem);
 function Banner() {
   return (
     <div className="Banner">
 
       <div className="Banner-body">
 
-<p>{quotesArray[1].quote}</p>
         <div className="Banner-body-quoteOfTheDay">
-          {quotesArray.map((item, key)=>{
-            return(
-
-              <div className="Banner-body-quote">
-                <h1 className="Banner-body-quote-main">" {item.quote} "</h1>
-                <h2 className="Banner-body-quote-author">by {item.author}</h2>
-                <p className="Banner-body-quote-publisher">provided by {item.publisher}</p>
-              </div>
-
-            )
-          })}
+          {
+            <div className="Banner-body-quote">
+              <h1 className="Banner-body-quote-main">" {quotesArrayItem.quote} "</h1>
+              <h2 className="Banner-body-quote-author">by {quotesArrayItem.author}</h2>
+              <p className="Banner-body-quote-publisher">provided by {quotesArrayItem.publisher}</p>
+            </div>
+          }
         </div>
       </div>
     </div>
