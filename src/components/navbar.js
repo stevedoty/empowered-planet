438775ba257cb6804f9c2feada084c2076589aae
Steve Doty <stevedoty16@gmail.com>
2022-03-03 08:20:19 -0800

    -

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,6 @@ import brandIcon from "../images/imports/planet.ico"
 
 function Navbar() {
   return (
-
     <div id="Navbar" className="Navbar">
         <div id="Navbar-filter" className="Navbar-filter"></div><div id="Navbar-filter" className="Navbar-filter"></div><div id="Navbar-filter" className="Navbar-filter"></div><div id="Navbar-filter" className="Navbar-filter"></div><div id="Navbar-filter" className="Navbar-filter"></div><div id="Navbar-filter" className="Navbar-filter"></div>
           <a href="" className="Navbar-item"><img src={brandIcon} className="Navbar-brand-icon" alt=""/></a>
