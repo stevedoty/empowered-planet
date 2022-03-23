bf04a1552a644b0a76df570fda1bf38555e50a7b
Steve Doty <stevedoty16@gmail.com>
2022-03-23 10:38:30 -0700

    -

---
+++ src/components/parallax/parallax.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Parallax extends Component {
+
+  render() {
+    return (
+      <div className="parallax">
+
+      </div>
+    );
+  }
+}
+
+export default Parallax;
