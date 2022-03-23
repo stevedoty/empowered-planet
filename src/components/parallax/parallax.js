22a902e4ebc24f98ff39f7acdb06b25937add30e
Steve Doty <stevedoty16@gmail.com>
2022-03-23 10:38:59 -0700

    parallax demo made

--- src/components/parallax/parallax.js
+++ src/components/parallax/parallax.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react';
 
+let twoDimensions = {
+  position: "relative",
+  height: "100vh",
+  transformStyle: "preserve-3d",
+  transition: "transform 0.5s",
+  transitionProperty: "transform",
+  transitionDuration: "0.5s",
+  transitionTimingFunction: "ease",
+  transitionDelay: "0s",
+}
+let threeDimensions = {
+  transform:"translate3d(700px,0,-800px)rotateY(30deg)"
+}
 
 class Parallax extends Component {
 
