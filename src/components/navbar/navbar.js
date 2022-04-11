f32bf88f89becb5ab362a834ce4a94471c6d24af
Steve Doty <stevedoty16@gmail.com>
2022-04-11 09:41:28 -0700

    -

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,15 +4,23 @@ import './navbar.css'
 import brandIcon from "../../images/imports/planet.ico"
 
 function Navbar() {
-  const [style, setStyle] = useState("");
-
+  let [style, setStyle] = useState({display:"none"})
+  let hidden = true;
   return (
     <div id="Navbar" className="Navbar">
         <a href="" className="Navbar-item"><img src={brandIcon} className="Navbar-brand-icon" alt=""/></a>
         <a href="" className="Navbar-item Navbar-item-index">Empowered Planet</a>
         <a href="#Library" className="Navbar-item Navbar-item-index">Library</a>
         <a href="#Store" className="Navbar-item Navbar-item-index">Store</a>
-        <div className="Navbar-item Navbar-item-index">Donate</div><BrandLinks />
+        <div id="navbar-donate-button" className="Navbar-item Navbar-item-index"
+          onClick={()=>{
+            if(hidden){
+              console.log(hidden);hidden=false
+            }else{console.log(hidden);hidden=true}
+          }}>Donate</div>
+        <div style={style}>
+          <BrandLinks />
+        </div>
     </div>
   );
 }
